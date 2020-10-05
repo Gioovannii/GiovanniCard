bf1a303593479fd1ff44185897f6c5f4bc005942
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-10-05 22:19:44 +0800

    challenge divide

--- GiovanniCard/ContentView.swift
+++ GiovanniCard/ContentView.swift
@@ -9,8 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.47, green: 0.88, blue: 0.56)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("gio")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, style: /*@START_MENU_TOKEN@*/StrokeStyle()/*@END_MENU_TOKEN@*/)
+                        
+                    )
+                    
+
+                Text("Giovanni Gaffé")
+                .font(.custom("Pacifico-Regular", size: 40))
+                .bold()
+                .foregroundColor(.white)
+                    .padding()
+                Text("iOS Developper")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                
+                HStack {
+                    
+                }
+                
+            }
+        }
     }
 }
 
