b3bada39d3614ee5d33e24e0f5f25fbfee991fba
Giovanni Gaffe <gaffejonathan@ymail.com>
2020-10-06 13:27:27 +0800

    Projet done extracts view and display on top of each others

--- GiovanniCard/ContentView.swift
+++ GiovanniCard/ContentView.swift
@@ -12,14 +12,14 @@ struct ContentView: View {
         ZStack {
             Color(red: 0.47, green: 0.88, blue: 0.56)
                 .edgesIgnoringSafeArea(.all)
-            VStack {
+            VStack(alignment: .center) {
                 Image("gio")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 150.0, height: 150.0)
                     .clipShape(Circle())
                     .overlay(
-                        Circle().stroke(Color.white, style: /*@START_MENU_TOKEN@*/StrokeStyle()/*@END_MENU_TOKEN@*/)
+                        Circle().stroke(Color.white, style: StrokeStyle(lineWidth: 5, lineCap: CGLineCap(rawValue: 0)!, lineJoin: CGLineJoin(rawValue: 0)!, miterLimit: 0, dash: [1, 0], dashPhase: 0))
                         
                     )
                     
@@ -32,12 +32,11 @@ struct ContentView: View {
                 Text("iOS Developper")
                     .foregroundColor(.white)
                     .font(.system(size: 25))
+                
                 Divider()
+                InfoView(text: "+886 938320050", imageName: "phone.fill")
                 
-                HStack {
-                    
-                }
-                
+                InfoView(text: "gaffejonathan@ymail.com", imageName: "envelope.fill")
             }
         }
     }
---
+++ GiovanniCard/InfoView.swift
@@ -0,0 +1,33 @@
+//
+//  InfoView.swift
+//  GiovanniCard
+//
+//  Created by Giovanni Gaffé on 2020/10/6.
+//
+
+import SwiftUI
+
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+            }).padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
